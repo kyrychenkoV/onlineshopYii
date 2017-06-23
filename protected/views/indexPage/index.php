dbb0aabfe0f888916ef558001a3c5a0423e83267
kyrychenkoV <keryvn@gmail.com>
2017-06-23 12:38:59 +0300

    search

--- protected/views/indexPage/index.php
+++ protected/views/indexPage/index.php
@@ -25,30 +25,30 @@ $('.search-form form').submit(function(){
 ?>
 
 <h1>Posts</h1>
-
+<!---->
 <?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
     <?php $this->renderPartial('_search',array(
         'model'=>$model,
     )); ?>
-</div><!-- search-form -->
-
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-    'id'=>'post-grid',
-    'dataProvider'=>$model->search(),
-    'filter'=>$model,
-    'columns'=>array(
-        'id',
-        'title',
-        'text',
-        'id_lists',
-        'image',
-        'date_create',
-        array(
-            'class'=>'CButtonColumn',
-        ),
-    ),
-)); ?>
+<!--</div><!-- search-form -->-->
+<!---->
+<?php //$this->widget('zii.widgets.grid.CGridView', array(
+//    'id'=>'post-grid',
+//    'dataProvider'=>$model->search(),
+//    'filter'=>$model,
+//    'columns'=>array(
+//        'id',
+//        'title',
+//        'text',
+//        'id_lists',
+//        'image',
+//        'date_create',
+//        array(
+//            'class'=>'CButtonColumn',
+//        ),
+//    ),
+//)); ?>
 
 
 <?php $this->widget('zii.widgets.CListView', array(
--- protected/views/indexPage/indexPage.php
+++ protected/views/indexPage/indexPage.php
@@ -39,26 +39,26 @@ $('.search-form form').submit(function(){
         'model'=>$model,
     )); ?>
 </div><!-- search-form -->
-
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-    'id'=>'post-grid',
-    'dataProvider'=>$model->search(),
-    'filter'=>$model,
-    'columns'=>array(
-        'id',
-        'title',
-        'text',
-        'id_lists',
-        'image',
-        'date_create',
-        /*
-        'last_status_change',
-        */
-        array(
-            'class'=>'CButtonColumn',
-        ),
-    ),
-)); ?>
+<!---->
+<?php //$this->widget('zii.widgets.grid.CGridView', array(
+//    'id'=>'post-grid',
+//    'dataProvider'=>$model->search(),
+//    'filter'=>$model,
+//    'columns'=>array(
+//        'id',
+//        'title',
+//        'text',
+//        'id_lists',
+//        'image',
+//        'date_create',
+//        /*
+//        'last_status_change',
+//        */
+//        array(
+//            'class'=>'CButtonColumn',
+//        ),
+//    ),
+//)); ?>
 
 <?php $this->widget('zii.widgets.CListView', array(
     'dataProvider'=>$dataProvider,
